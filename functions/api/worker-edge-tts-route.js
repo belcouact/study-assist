56d6870d658d3176063f24c6b80fb1370755bbed
alex <132571895+belcouact@users.noreply.github.com>
2025-05-10 11:35:17 +0800

    1

--- functions/api/worker-edge-tts-route.js
+++
@@ -1,8 +0,0 @@
-/**
- * Route handler for Worker Edge TTS API
- * This module forwards requests to the worker-edge-tts handler
- */
-
-import { onRequest } from './worker-edge-tts';
-
-export { onRequest };
